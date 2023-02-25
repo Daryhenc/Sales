d89a035d8b2b9f940661b88f3c1f29a117ea517d
Daryhen <daryhenc88@gmail.com>
2023-02-25 15:56:43 -0500

    add data context

---
+++ Sales.Shared/Entities/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Shared.Entities
+{
+    public class Country
+    {
+        public int CountryId { get; set; }
+        [Display(Name = "País")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
+        public string Name { get; set; } = null!;   
+    }
+}
